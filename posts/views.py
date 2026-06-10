d30b12f52aa29439092eea91273f03ea0105ceb9
Rikopin0811 <suzuki.riko@dnti.co.jp>
2026-06-10 14:49:07 +0900

    ルーティングとビューの設定

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
-
+from django.views.generic import CreateView,ListView,DetailView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Post
 # Create your views here.
+class IndexView(ListView):
+  model = Post
+  template_name = 'posts/index.html'
+  context_object_name = 'posts'
+  ordering = '-created_at'
