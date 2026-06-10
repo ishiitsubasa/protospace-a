e0f4b7857f92454434f784ad0c96750de0062a57
Rikopin0811 <suzuki.riko@dnti.co.jp>
2026-06-10 14:49:37 +0900

    仮想環境

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/hidaka.taisei/Desktop/protospace-a/.venv"
+set -gx VIRTUAL_ENV "/Users/suzuki.riko/Downloads/protospace-a/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
