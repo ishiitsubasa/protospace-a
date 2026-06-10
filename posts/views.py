915c4891e8abc23e7a7de8d44a84b3aff1a1a383
Rikopin0811 <suzuki.riko@dnti.co.jp>
2026-06-10 14:56:42 +0900

    Update views.py

--- posts/views.py
+++ posts/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.views.generic import CreateView,ListView,DetailView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Post
-# Create your views here.
+
 class IndexView(ListView):
   model = Post
   template_name = 'posts/index.html'
