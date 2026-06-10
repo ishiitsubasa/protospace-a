89410de9775a53a09b81e527523eb54d86f808b3
HidakaTaisei <hidaka.taisei@dnti.co.jp>
2026-06-10 17:33:32 +0900

    微調整

--- users/models.py
+++ users/models.py
@@ -22,7 +22,7 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractBaseUser):
     email = models.EmailField(unique=True, blank=False, null=False)
     nickname = models.CharField(max_length=10, blank=False, null=False)
-    profile = models.CharField(ax_length=100,blank=False, null=False)
+    profile = models.CharField(max_length=100,blank=False, null=False)
     belonging = models.TextField(blank=False, null=False)
     role = models.CharField(blank=False, null=False)
 
