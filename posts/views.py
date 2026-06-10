25fec6ec5d89819d0553c02e4da66e0a3a5205be
Rikopin0811 <suzuki.riko@dnti.co.jp>
2026-06-10 15:16:41 +0900

    ビューの設定

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+class IndexView(ListView):
+  model = Post
+  template_name = 'posts/index.html'
+  context_object_name = 'posts'
+  ordering = '-created_at'
