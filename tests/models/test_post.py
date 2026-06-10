85243ed5825e617850ac98567c71cfc0d3ae1b24
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-06-10 17:19:01 +0900

    テストコード

--- tests/models/test_post.py
+++ tests/models/test_post.py
@@ -4,7 +4,7 @@ from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
 
 from posts.models import Post
-from .factories import PostFactory, UserFactory
+from tests.factories.test_posts import PostFactory, UserFactory
 
 User = get_user_model()
 
