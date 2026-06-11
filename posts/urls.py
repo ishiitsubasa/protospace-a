a05b5996107af1d91bf4f41c273c741d70bb5033
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-06-11 12:00:30 +0900

    pathとviewsに追加

--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path,include
-from .views import PostCreateView
+from .views import PostCreateView,PostDeleteView
 
 app_name='Posts'
 
 urlpatterns = [
   
   path('posts/create/',PostCreateView.as_view(),name='create'),
+   path('posts/<int:pk>/delete',PostDeleteView.as_view(),name='delete'),
 
 
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import CreateView
+from django.views.generic import CreateView,DeleteView
 from django.urls import reverse_lazy
 from .models import Post
 from .forms import PostForm
@@ -17,4 +17,8 @@ class PostCreateView(CreateView):
     post.user = self.request.user
     post.save()
     return super().form_valid(form)
+
+class  PostDeleteView(DeleteView):
+  model=Post
+  success_url=reverse_lazy('Posts:index')
   
