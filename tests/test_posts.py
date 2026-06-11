1405508f446c0ccb2dda694d63592f71eac3767c
Rikopin0811 <suzuki.riko@dnti.co.jp>
2026-06-11 09:53:37 +0900

    コードレビュー修正

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,39 +1,20 @@
 from django.test import TestCase
 from django.urls import reverse
-from .models import Post
-from django.contrib.auth import get_user_model
-
-User = get_user_model()
+from tests.factories.posts import PostFactory
 
 class PostIndexViewTest(TestCase):
 
     def setUp(self):
-        # テスト用ユーザーを作成
-        self.user = User.objects.create_user(
-            nickname='testuser',
-            password='testpassword'
-        )
-        # テスト用の投稿を作成
-        self.post = Post.objects.create(
-            name='テスト投稿',
-            catchphrase='テストキャッチコピー',
-            concept='テストコンセプト',
-            user=self.user
-            # imageは任意なので省略OK
-            # created_at, updated_atは自動で入るので省略OK
-        )
+        self.post = PostFactory()
 
     def test_index_status_code(self):
-        # ページが200で返ってくるか
         response = self.client.get(reverse('Posts:index'))
         self.assertEqual(response.status_code, 200)
 
     def test_index_template(self):
-        # 正しいテンプレートが使われているか
         response = self.client.get(reverse('Posts:index'))
         self.assertTemplateUsed(response, 'posts/index.html')
 
     def test_index_contains_post(self):
-        # 投稿が一覧に表示されているか
         response = self.client.get(reverse('Posts:index'))
-        self.assertContains(response, 'テスト投稿')
+        self.assertContains(response, self.post.name)
