445f5f9ab83abd395e82e3f8d7308a91485acc5b
HidakaTaisei <hidaka.taisei@dnti.co.jp>
2026-06-11 13:10:29 +0900

    HTMLの修正

--- users/models.py
+++ users/models.py
@@ -7,13 +7,12 @@ class CustomUserManager(BaseUserManager):
   def create_user(self,email,nickname,password=None,**extra_fields):
     if not email:
       raise ValueError('Users must have an email address')
-    user = self.model(
-            email=self.normalize_email(email),
-        )
-    if password and len(password) < 6:
-       raise ValidationError('パスワードは6文字以上で入力してください。')
+    if password and len(password) < 8:
+       raise ValidationError('パスワードは8文字以上で入力してください。')
     
-    user = self.model(email=email, nickname=nickname, **extra_fields)
+    user = self.model(email=self.normalize_email(email),
+                       nickname=nickname, 
+                       **extra_fields)
     user.set_password(password)
     user.save(using=self._db)
     return user
@@ -22,9 +21,9 @@ class CustomUserManager(BaseUserManager):
 class CustomUser(AbstractBaseUser):
     email = models.EmailField(unique=True, blank=False, null=False)
     nickname = models.CharField(max_length=10, blank=False, null=False)
-    profile = models.CharField(blank=False, null=False)
-    belonging = models.CharField(blank=False, null=False)
-    role = models.CharField(blank=False, null=False)
+    profile = models.CharField(max_length=100,blank=False, null=False)
+    belonging = models.TextField(blank=False, null=False)
+    role = models.CharField(max_length=15,blank=False, null=False)
 
 
     USERNAME_FIELD = 'email'
