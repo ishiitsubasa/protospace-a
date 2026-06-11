1d8e7daf62339a208d51d5e9316c89fd29713900
HidakaTaisei <hidaka.taisei@dnti.co.jp>
2026-06-11 13:15:45 +0900

    インデント修正

--- users/models.py
+++ users/models.py
@@ -15,19 +15,16 @@ class CustomUserManager(BaseUserManager):
             nickname=nickname,
             **extra_fields
         )
-      
-    user = self.model(email=email, nickname=nickname, **extra_fields)
-    user.set_password(password)
-    user.save(using=self._db)
-    return user
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
 
 class CustomUser(AbstractBaseUser):
     email = models.EmailField(unique=True, blank=False, null=False)
     nickname = models.CharField(max_length=10, blank=False, null=False)
-    profile = models.CharField(max_length=100,blank=False, null=False)
+    profile = models.CharField(max_length=100, blank=False, null=False)
     belonging = models.TextField(blank=False, null=False)
     role = models.CharField(max_length=15,blank=False, null=False)
-
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['nickname']
