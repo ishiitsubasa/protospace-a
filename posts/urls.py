5f72513fa664649cafb9dce20b6af1a4c7bcc865
Rikopin0811 <suzuki.riko@dnti.co.jp>
2026-06-11 13:11:06 +0900

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -5,7 +5,7 @@ app_name='Posts'
 
 urlpatterns = [
   path('', IndexView.as_view(), name='index'),
-  path('detail/int:pk/', DetailView.as_view(), name='detail')
+  path('detail/int:pk/', DetailView.as_view(), name='detail'),
   path('posts/<int:pk>',PostDetailView.as_view(), name='detail'),
 
 ]
