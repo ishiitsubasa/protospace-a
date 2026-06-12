bf6d83f55ad7004593ac33f2af9703198f41d6fb
taretare17 <sano.yuta@dnti.co.jp>
2026-06-12 11:09:30 +0900

    urlsを一新
    urlsに別のファイルを書いていたので書き直し

--- comments/urls.py
+++ comments/urls.py
@@ -1,13 +1,8 @@
 from django.urls import path
-from .views import CreateView,IndexView, DetailView, UpdateView, DeleteView
+from . import views
 
-app_name='Posts'
+app_name = "comments"
 
 urlpatterns = [
-  path('', IndexView.as_view(), name='index'),
-  path('posts/create/',CreateView.as_view(),name='create'),
-  path('posts/<int:pk>',DetailView.as_view(), name='detail'),
-   path('posts/<int:pk>/update', UpdateView.as_view(), name='update'),
-  path('posts/<int:pk>/delete',DeleteView.as_view(),name='delete'),
-
+    path('<int:pk>/comment/', views.CommentCreateView.as_view(), name='create'),
 ]
