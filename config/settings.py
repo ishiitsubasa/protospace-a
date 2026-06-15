3349a2a2e0c0518f91eba275e3d1a6920c1b20f3
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-06-15 10:58:43 +0900

    httpsの追加

--- config/settings.py
+++ config/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://protospace-a-e5z5.onrender.com']
 
 
 # Application definition
