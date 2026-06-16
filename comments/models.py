2ff0eed2865e351ece8a91368679f090b7e87bf1
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-06-16 15:25:35 +0900

    アプリケーション作成、models作成終了です

--- comments/models.py
+++ comments/models.py
@@ -1,12 +1,22 @@
 from django.db import models
 from django.conf import settings
 
+
 class Comment(models.Model):
     class Meta:
         db_table = 'comments'
 
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
-    post = models.ForeignKey('posts.Post', on_delete=models.CASCADE, null=False)
-    text = models.TextField(null=False, blank=False)
+    user       = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=False)
+    post       = models.ForeignKey('posts.Post', on_delete=models.CASCADE, null=False)
+    text       = models.TextField(null=False, blank=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+        if self.user != self.post.user:
+            from notifications.models import Notification  # 循環import防止のため関数内でimport
+            Notification.objects.create(
+                user=self.post.user,
+                comment=self
+            )
