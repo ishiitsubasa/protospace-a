4ec9d4fd010771e0ebace2190a5fa02743d3a825
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-06-17 13:13:22 +0900

    再々修正

--- build.sh
+++ build.sh
@@ -3,8 +3,5 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate --run-syncdb --fake-initial || true
-python manage.py migrate notifications zero --fake
 python manage.py migrate comments zero --fake
-python manage.py migrate posts zero --fake
-python manage.py migrate --fake-initial
+python manage.py migrate
