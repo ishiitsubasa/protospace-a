e52e7a2e1800fe538cffe4f6febc6e4293839d1e
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-06-17 10:35:35 +0900

    build.shのへんこう

--- build.sh
+++ build.sh
@@ -3,6 +3,5 @@
 set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
-python manage.py migrate posts --fake-initial
-python manage.py migrate comments --fake-initial
+python manage.py migrate comments zero --fake
 python manage.py migrate
