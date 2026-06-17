5fd3514289cae65cac5ffd487a6d544cc39cf6bd
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-06-17 11:32:31 +0900

    render1回目

--- notifications/migrations/0001_initial.py
+++ notifications/migrations/0001_initial.py
@@ -10,7 +10,7 @@ class Migration(migrations.Migration):
     initial = True
 
     dependencies = [
-        #('comments', '0001_initial'),
+        ('comments', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
--- notifications/models.py
+++ notifications/models.py
@@ -1,15 +1,15 @@
 from django.db import models
 from django.conf import settings
-from comments.models import Comment
 
 class Notification(models.Model):
-  class Meta:
-    db_table='notifications'
-  user=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='notifications')
-  comment=models.OneToOneField(Comment, on_delete=models.CASCADE)
-  read_at    = models.DateTimeField(null=True, blank=True)
-  created_at = models.DateTimeField(auto_now_add=True)
-  @property
-  def is_read(self):
-        return self.read_at is not None
+    class Meta:
+        db_table = 'notifications'
+    
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='notifications')
+    comment = models.OneToOneField('comments.Comment', on_delete=models.CASCADE)  # 文字列参照に変更
+    read_at = models.DateTimeField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
+    @property
+    def is_read(self):
+        return self.read_at is not None
