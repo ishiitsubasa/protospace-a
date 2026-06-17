2b138dac972e951d448452643fca13b724a636aa
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-06-17 10:09:20 +0900

    データベースの再編

--- posts/migrations/0003_like.py
+++ posts/migrations/0003_like.py
@@ -7,26 +7,23 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    initial = True
-
     dependencies = [
-        ('posts', '0003_like'),
+        ('posts', '0002_alter_post_name'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
-            name='Comment',
+            name='Like',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('text', models.TextField()),
                 ('created_at', models.DateTimeField(auto_now_add=True)),
-                ('updated_at', models.DateTimeField(auto_now=True)),
-                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to='posts.post')),
+                ('post', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, related_name='likes', to='posts.post')),
                 ('user', models.ForeignKey(on_delete=django.db.models.deletion.CASCADE, to=settings.AUTH_USER_MODEL)),
             ],
             options={
-                'db_table': 'comments',
+                'db_table': 'likes',
+                'unique_together': {('user', 'post')},
             },
         ),
     ]
