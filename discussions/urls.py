6f7db5159e82a0c27c88b5273402206300474657
HidakaTaisei <hidaka.taisei@dnti.co.jp>
2026-06-18 17:43:46 +0900

    ディスカッション詳細機能

--- discussions/urls.py
+++ discussions/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from . import views
-from views import DiscussionIndexView,DiscussionCreateView
 
-app_name='discussions'
+app_name = 'discussions'
 
 urlpatterns = [
-    path('discussions/create',DiscussionCreateView.as_view(),name='create'),
+    path('create/<int:post_pk>/', views.DiscussionCreateView.as_view(), name='create'),
+    path('<int:pk>/', views.DiscussionDetailView.as_view(), name='detail'),
 ]
--- discussions/views.py
+++ discussions/views.py
@@ -1,21 +1,17 @@
-from django.shortcuts import render
-from django.urls import reverse_lazy, reverse
-from django.views.generic import ListView,CreateView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView,DetailView
 from .models import Topic
-from django.shortcuts import get_object_or_404,redirect
+from django.shortcuts import get_object_or_404
 from posts.models import Post
-from comments.models import Count
-from django .forms import DiscussionTopicForm
+from .forms import DiscussionForm
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-
-
+from comments.forms import CommentForm
 
 
 class DiscussionCreateView(LoginRequiredMixin, CreateView):
     """議題作成"""
     model = Topic
-    form_class = DiscussionTopicForm
+    form_class = DiscussionForm
     success_url = reverse_lazy('Posts:detail')
     
     def form_valid(self, form):
@@ -25,8 +21,17 @@ class DiscussionCreateView(LoginRequiredMixin, CreateView):
         return super().form_valid(form)
 
 
+class DiscussionDetailView(DetailView):
+    model = Topic
+    template_name = 'discussions/detail.html'
 
-
+    def get_context_data(self, **kwargs):
+        ctx = super().get_context_data(**kwargs)
+        ctx['discussions'] = self.object        # Topic
+        ctx['post'] = self.object.post          # Post（include先が使う）
+        ctx['comments'] = ...
+        ctx['form'] = CommentForm()
+        return ctx
      
      
      
