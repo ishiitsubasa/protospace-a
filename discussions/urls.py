949230a8405a0fcc5e99c00d4de48947de27649c
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-06-18 18:04:57 +0900

    完了かもしれない

--- discussions/urls.py
+++ discussions/urls.py
@@ -5,5 +5,5 @@ app_name = 'discussions'
 
 urlpatterns = [
     path('posts/<int:post_pk>/discussions/', views.DiscussionIndexView.as_view(), name='index'),
-    path('posts/<int:post_pk>/discussions/create', views.DiscussionCreateView.as_view(), name='create'),
+    path('posts/<int:post_pk>/discussions/create/', views.DiscussionCreateView.as_view(), name='create'),
 ]
