ff52fc2bd9bb7adf968557507e7cbdd7ff733b7d
HidakaTaisei <hidaka.taisei@dnti.co.jp>
2026-06-19 11:37:14 +0900

    コンフリクト解消

--- discussions/urls.py
+++ discussions/urls.py
@@ -5,5 +5,6 @@ app_name = 'discussions'
 
 urlpatterns = [
     path('create/<int:post_pk>/', views.DiscussionCreateView.as_view(), name='create'),
+    
     path('<int:pk>/', views.DiscussionDetailView.as_view(), name='detail'),
 ]
