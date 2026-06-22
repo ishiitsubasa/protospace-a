9f905da7bf1e81279d61b33ba96e48af09469ce3
ishiitsubasa <ishii.tsubasa@dnti.co.jp>
2026-06-22 10:04:23 +0900

    マイグレーション変更

--- posts/migrations/0003_likes.py
+++ posts/migrations/0003_likes.py
@@ -2,7 +2,9 @@ from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
 
+
 class Migration(migrations.Migration):
+
     dependencies = [
         ('posts', '0002_alter_post_name'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
