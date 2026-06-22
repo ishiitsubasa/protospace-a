1c6c844dda413f9115725738e0669a5d4ea9c097
HidakaTaisei <hidaka.taisei@dnti.co.jp>
2026-06-22 12:31:56 +0900

    パスの修正

--- comments/urls.py
+++ comments/urls.py
@@ -1,9 +1,7 @@
-# comments/urls.py
 from django.urls import path
 from . import views
 
 app_name = "comments"
-
 urlpatterns = [
     path('<int:pk>/comment/', views.CommentCreateView.as_view(), name='create'),
     path('<int:pk>/comment/topic/<int:topic_pk>/', views.CommentCreateView.as_view(), name='create_for_topic'),
